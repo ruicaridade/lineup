28bb321bc340c3f9f9d04e22488aecbf2f25c0c3
Rui Caridade <ruidanielcaridade@gmail.com>
2021-03-07 17:12:47 +0000

    Feat(backend): Implement `users` views

--- backend/lineup/auth/views/users.py
+++ backend/lineup/auth/views/users.py
@@ -1,4 +1,9 @@
-from flask import Blueprint
+import json
+
+from flask import Blueprint, Response
+
+from lineup.auth.repositories import users
+from lineup.core.exceptions import NotFoundError
 
 blueprint = Blueprint(
     name='users',
@@ -8,19 +13,31 @@ blueprint = Blueprint(
 
 @blueprint.route('/')
 def list_users():
+    repository = users.Repository()
+    items, total, page = repository.all(
+        limit=123,
+        page=0
+    )
     return {
-        'items': [
-            {
-                'id': 1
-            }
-        ],
-        'page': 1,
-        'total': 100
+        'items': [x.to_dict(as_camel_case=True) for x in items],
+        'page': page,
+        'total': total
     }
 
 
 @blueprint.route('/<int:user_id>')
 def retrieve_user(user_id: int):
-    return {
-        'id': user_id
-    }
+    repository = users.Repository()
+
+    try:
+        item = repository.find_by_id(user_id)
+    except NotFoundError:
+        return Response(
+            status=404,
+            content_type='application/json',
+            response=json.dumps({
+                'error': f'Instance of id `{user_id}` not found.'
+            })
+        )
+
+    return item.to_dict(as_camel_case=True)
