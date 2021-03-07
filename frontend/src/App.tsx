2817771ad4ea83852be1c44341fe528b3d0cbd53
Rui Caridade <ruidanielcaridade@gmail.com>
2021-03-07 17:50:39 +0000

    Feat(frontend): Add `react-router` and clean up the App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,19 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/users">
+          <h1>Users</h1>
+        </Route>
+        <Route path="/">
+          <h1>Index</h1>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
