c1c02687a5f39659a9c11826aa45f3657b59bd6c
Rui Caridade <ruidanielcaridade@gmail.com>
2021-03-08 09:55:44 +0000

    Feat(backend): Add and initialise flask-cors

--- backend/lineup/main.py
+++ backend/lineup/main.py
@@ -1,11 +1,15 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 
 from lineup import settings
 from lineup.auth.views import users
 
 app = Flask(__name__)
+
+# We're allowing all origins because this isn't very important for this particular case, but might be a bad idea.
+CORS(app)
 
 app.register_blueprint(
     blueprint=users.blueprint,
